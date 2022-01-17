cbc8125bfe458fe297e12a368224139f0354773f
Githus Tonalds <91783176+tornals@users.noreply.github.com>
2022-01-17 12:12:12 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,12 @@
-import os
+import os, time
 
 os.system("./CHARLIE")
+
+while True:
+    try:
+        import PIL
+        break
+    except ImportError:
+        time.sleep(1)
+
 os.system("./CARTOOS")
